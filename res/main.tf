2a7dadf57c63c475d7f14a080f8b713e1820f899
Victor Shumskiy <krishna2003@gmail.com>
2024-10-31 14:18:39 +0300

    Add files via upload

---
+++ res/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = "~>1.8.4" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+
+provider "docker" {}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+#однострочный комментарий
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+#/*
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "hello_world_${random_password.random_string.result}"
+
+  ports {
+    internal = 80
+    external = 9090
+  }
+}
+#*/
